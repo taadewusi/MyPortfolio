7ad614d5db7d34ee7da9a04cd59e13e61a02dd1b
taadewusi <41637140+taadewusi@users.noreply.github.com>
2022-05-08 16:12:18 +0100

    fxi

--- Vendor/contact-mailer.php
+++ Vendor/contact-mailer.php
@@ -11,7 +11,7 @@ if($_POST)
 
     $mail = new PHPMailer();
 
-    $your_email = "youremail@website.com";
+    $your_email = "me@taiwoadewusi.com";
 
 
     //check if its an ajax request, exit if not
@@ -267,13 +267,13 @@ if($_POST)
 
 
     //Server settings
-//    $mail->isSMTP();                                            // Send using SMTP
-//    $mail->Host       = 'smtp.googlemail.com';                    // Set the SMTP server to send through
-//    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-//    $mail->Username   = 'website@gmail.com';                     // SMTP username
-//    $mail->Password   = 'your password';                         // SMTP password
-//    $mail->SMTPSecure = 'TLS';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-//    $mail->Port       = 587;                                    // TCP port to connect to
+    $mail->isSMTP();                                            // Send using SMTP
+   $mail->Host       = 'smtp.googlemail.com';                    // Set the SMTP server to send through
+   $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+   $mail->Username   = 'website@gmail.com';                     // SMTP username
+   $mail->Password   = 'your password';                         // SMTP password
+   $mail->SMTPSecure = 'TLS';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+   $mail->Port       = 587;                                    // TCP port to connect to
 
     //Recipients
     $mail->setFrom($user_Email,$user_Name);
